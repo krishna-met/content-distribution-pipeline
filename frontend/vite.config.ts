d789b2c9373515d03ecb19037104b590149e7fd1
krishna-met <krishnab.mca_iom@bkc.met.edu>
2025-09-28 19:07:20 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.',
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  define: {
-    // This provides a shim for process.env
-    // which is a Node.js-specific object
-    "process.env": {}
+  plugins: [react()],
+  root: './',
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
   }
-});
+})
