a7f76d700dbf20f3ba61c31a39da8445d17a53a7
krishna-met <krishnab.mca_iom@bkc.met.edu>
2025-09-28 19:31:11 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  base: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html'
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
     }
+  },
+  build: {
+    outDir: 'dist'
   }
 })
