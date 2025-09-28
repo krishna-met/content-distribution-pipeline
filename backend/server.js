ea9ecaf5be4cff95ae6a014d30cc1e6639b51f18
krishna-met <krishnab.mca_iom@bkc.met.edu>
2025-09-28 20:47:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import ContentAPIService from './services/contentService.js';
 
 // ES modules fix for __dirname
